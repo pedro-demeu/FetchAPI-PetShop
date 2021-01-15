f986368a4a0829183e9ee6d69a71a575db576509
pedro demeu <demeu.pedro@gmail.com>
2021-01-14 23:00:30 -0300

    uso de template string em javascript para obter e renderizar dados

--- cliente.js
+++ cliente.js
@@ -4,3 +4,15 @@ const informacoesClientes = [
         nome: "Jairo"
     }
 ]
+
+const conteudo =
+ `<tr>
+    <td>${informacoesClientes[0].cpf}</td>
+    <td>${informacoesClientes[0].nome}</td>
+  </tr>`;
+
+console.log(conteudo)
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]")
+
+corpoTabela.innerHTML = conteudo;
