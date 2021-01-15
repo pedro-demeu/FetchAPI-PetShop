a2bb6a1988f12184f7860d00b1c684c831645dfd
Pedro Demeu <demeu.pedro@gmail.com>
2021-01-15 11:21:30 -0300

    criado metodo para realizar POST de dados no banco

--- api/cliente.js
+++ api/cliente.js
@@ -1,4 +1,4 @@
-listarClientes = () => {
+const listarClientes = () => {
   return fetch('http://localhost:4000/clientes')
     .then((res) => {
       return res.json();
@@ -6,4 +6,20 @@ listarClientes = () => {
     .then((json) => {
       return json;
     });
+};
+
+const cadastrarClientes = (nome, cpf) => {
+  const json = JSON.stringify({
+    nome: nome,
+    cpf: cpf,
+  });
+  return fetch('http://localhost:4000/clientes/cliente', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: json,
+  }).then((resp) => {
+    return resp.body;
+  });
 };
