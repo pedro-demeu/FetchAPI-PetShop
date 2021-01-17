ad6b2b3cfe1ff46a5ad70e4547115bfe4cd0b2a8
Pedro Demeu <demeu.pedro@gmail.com>
2021-01-17 13:52:45 -0300

    adicionado função para puxar dados via id e setar nos campos

--- listagem-clientes.js
+++ listagem-clientes.js
@@ -12,6 +12,7 @@ const renderizarCliente = (cpf, nome, id) => {
     <td>${nome}</td>
     <button type="button" class="btn btn-danger" 
     onClick="removerCliente(${id})">Excluir</button>
+    <a href="edita-clientes.html?id=${id}"><button type="button" class="btn btn-info">Editar</button></a>
 `;
   linha.innerHTML = conteudo;
 
