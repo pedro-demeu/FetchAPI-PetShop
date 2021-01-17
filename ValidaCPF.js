c697e1beb486fabc2335c8d17ab5569e38bfe854
Pedro Demeu <demeu.pedro@gmail.com>
2021-01-17 13:00:17 -0300

    Criamos uma função capaz de Validar CPFs

--- ValidaCPF.js
+++ ValidaCPF.js
@@ -1,4 +1,4 @@
-function VerificarCPF(cpf) {
+function VerificarCPFInvalidos(cpf) {
   //Cria um array de CPFs inválidos e faz uma verificação Booleana
   const cpfsInvalidos = [
     '00000000000',
@@ -13,6 +13,7 @@ function VerificarCPF(cpf) {
   ];
   return cpfsInvalidos.indexOf(cpf) === -1;
 }
+
 function somarNumerosDoCPF(cpf, totalDeDigitos, peso) {
   let soma = 0;
 
@@ -27,5 +28,38 @@ function VerificarDigito(cpf, totalDeDigitos, peso, digitoDeVerificacao) {
   const soma = somarNumerosDoCPF(cpf, totalDeDigitos, peso);
   const resto = (soma * 10) % 11;
 
-  return resto;
+  return resto === digitoDeVerificacao;
+}
+
+function VerificaPrimeiroDigito(cpf) {
+  const peso = 11;
+  const totalDeDigitosPrimeiraParte = 9;
+  const digitoDeVerificacao = parseInt(cpf.substring(9, 10));
+
+  return VerificarDigito(
+    cpf,
+    totalDeDigitosPrimeiraParte,
+    peso,
+    digitoDeVerificacao,
+  );
+}
+
+function VerificaSegundoDigito(cpf) {
+  const peso = 12;
+  const totalDeDigitosSegundaParte = 10;
+  const digitoDeVerificacao = parseInt(cpf.substring(10, 11));
+  return VerificarDigito(
+    cpf,
+    totalDeDigitosSegundaParte,
+    peso,
+    digitoDeVerificacao,
+  );
+}
+
+function validaCPF(cpf) {
+  return (
+    VerificaPrimeiroDigito(cpf) &&
+    VerificaSegundoDigito(cpf) &&
+    VerificarCPFInvalidos(cpf)
+  );
 }
